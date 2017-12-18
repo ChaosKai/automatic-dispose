665862022331d811cfdbea3a3b4d0cc41af557ed
ChaosKai <kai-waggeling@web.de>
2017-12-18 01:56:27 +0100

    Create mission-handling.js

---
+++ script/mission-handling.js
@@ -0,0 +1,28 @@
+$(document).ready(function()
+{
+    setInterval( AutomaticDispose_CheckMissionAttention, 2000 );
+});
+
+
+var AutomaticDispose_MissionWindows = {};
+
+function AutomaticDispose_CheckMissionAttention()
+{
+    var Missions = JSON.parse( localStorage.getItem("AutomaticDispose-Missions") );
+    var CurrentTime = Math.floor( new Date().getTime() / 1000 );
+    
+    if( Missions == null )
+        Missions = {};
+        
+    $.each(function(MissionID, Mission)
+    {
+        if( Mission.next_check > CurrentTime )
+        {
+            AutomaticDispose_MissionWindows[ Mission.id ] = window.open("https://www.leitstellenspiel.de/missions/" + Mission.id, "Mission " + Mission.id, "width=256,height=512");
+            
+            Missions[ Mission.id ]["last_check"] = CurrentTime;
+        }
+    });
+    
+    localStorage.setItem( "AutomaticDispose-Missions", JSON.stringify(Missions) );
+}
