cd643fa88819447f80eeb6e018af7509d40f16ac
ChaosKai <kai-waggeling@web.de>
2017-12-22 02:40:24 +0100

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -15,7 +15,16 @@
         styleElement.rel = "stylesheet";
         styleElement.href = "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700";
         document.body.appendChild(styleElement);
-        
+       
+//      -
+//      -           FontAwesome 5
+//      -
+
+        var styleElement = document.createElement("link");
+        styleElement.rel = "stylesheet";
+        styleElement.href = "https://use.fontawesome.com/releases/v5.0.2/js/all.js";
+        document.body.appendChild(styleElement);
+
 //      -
 //      -           Navbar Dashboard
 //      -
@@ -54,7 +63,7 @@
         }
         
 //      -
-//      -           Load Mission Handling
+//      -           Load Mission Opener
 //      -
         
         if (window.location.pathname === "/" || window.location.pathname === "/#")
@@ -63,6 +72,26 @@
             scriptElement.type = "text/javascript";
             scriptElement.src = "https://rawgit.com/ChaosKai/automatic-dispose/" + AutomaticDispose_Branch + "/script/mission-opener.js";
             document.body.appendChild(scriptElement);
+        }
+        
+//      -
+//      -           Load Dispatcher Overview
+//      -
+        
+        if (window.location.pathname === "/" || window.location.pathname === "/#")
+        {
+            var scriptElement = document.createElement("script");
+            scriptElement.type = "text/javascript";
+            scriptElement.src = "https://rawgit.com/ChaosKai/automatic-dispose/" + AutomaticDispose_Branch + "/script/dispatcher-overview.js";
+            document.body.appendChild(scriptElement);
+            
+            
+            var styleElement = document.createElement("link");
+            styleElement.rel = "stylesheet";
+            styleElement.type = "text/css";
+            styleElement.media = "screen";
+            styleElement.href = "https://rawgit.com/ChaosKai/automatic-dispose/" + AutomaticDispose_Branch + "/style/dispatcher-overview.css";
+            document.body.appendChild(styleElement);
         }
         
 //      -
