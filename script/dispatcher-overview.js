905479598bd6fa659424b52538ecb726665c4e7a
ChaosKai <kai-waggeling@web.de>
2017-12-22 03:17:36 +0100

    Update dispatcher-overview.js

--- script/dispatcher-overview.js
+++ script/dispatcher-overview.js
@@ -11,7 +11,8 @@ var DispatcherTable = {
 
 $(document).ready(function()
 {
-    
+    ADis_BuildDispatcherInterface();
+    setInterval(ADis_UpdateDispatcherMissions, 1000);
 });
 
 
@@ -21,8 +22,48 @@ function ADis_BuildDispatcherInterface()
     $("#adis-dispatcher-overview").append('<header></header>');
     $("#adis-dispatcher-overview").append('<div class="wrapper"></div>');
     
+    $("#adis-dispatcher-overview").css({ display: "none", opacity: "0" });
+    
     $("#adis-dispatcher-overview").find("header").append('<div id="adis-dispatcher-overview-title">Disponenten</div>');
     $("#adis-dispatcher-overview").find("header").append('<div id="adis-dispatcher-overview-close"><i class="far fa-times-circle"></a></div>');
     
-    $("#adis-dispatcher-overview").find(".wrapper").append('<div id="adis-dispatcher-dispatcher-table"></div>');
+    $("#adis-dispatcher-overview").find(".wrapper").append('<div id="adis-dispatcher-missions"></div>');
+    $("#adis-dispatcher-overview").find(".wrapper").append('<div id="adis-dispatcher-table"></div>');
+}
+
+
+function ADis_UpdateDispatcherMissions()
+{
+    var Missions = JSON.parse( localStorage.getItem("AutomaticDispose-Missions") );
+    var CurrentTime = Math.floor( new Date().getTime() / 1000 );
+    
+    if( Missions == null )
+        Missions = {};
+    
+    $("#adis-dispatcher-missions").find(".mission").each(function()
+    {
+        if( typeof Missions[ $(this).data("mission") ] === "undefined" )
+            $(this).remove();
+    });
+    
+    $.each(Missions, function(MissionID, Mission)
+    {
+        if( Mission.mode == "semi" || Mission.mode == localStorage.getItem("AutomaticDispose-Mode") )
+        {
+            if( $("#adis-dahboard-mission-" + MissionID).length == 0 )
+            {
+                $("#adis-dispatcher-missions").append('<div id="adis-dahboard-mission-' + MissionID + '" class="mission"></div>');
+                $("#adis-dahboard-mission-" + MissionID).append('<div class="name"></div>');
+                $("#adis-dahboard-mission-" + MissionID).append('<div class="countdown"></div>');
+            }
+            
+            $("#adis-dahboard-mission-" + MissionID).data("mission", MissionID);
+            $("#adis-dahboard-mission-" + MissionID).find(".name").html(Mission.name);
+            $("#adis-dahboard-mission-" + MissionID).find(".countdown").html( (Mission.next_check - CurrentTime) + "sek.");
+        }
+        else if( $("#adis-dahboard-mission-" + MissionID).length > 0 )
+        {
+            $("#adis-dahboard-mission-" + MissionID).remove();
+        }
+    });
 }
