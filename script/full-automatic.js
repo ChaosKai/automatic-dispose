3225227ae6505a701694c5f9c8baf40ddb85b90f
ChaosKai <kai-waggeling@web.de>
2017-12-23 06:19:33 +0100

    Update full-automatic.js

--- script/full-automatic.js
+++ script/full-automatic.js
@@ -1,1 +1,57 @@
 
+var ADis_Available_Missions = {};
+
+
+$(document).ready(function()
+{   
+    setInterval( ADis_FullAutomatic_CollectMissions, 10000 );
+    setInterval( ADis_GetAvailableMissions, 60000 );
+    
+    setTimeout( ADis_GetAvailableMissions, 1000 );
+});
+
+
+function ADis_GetAvailableMissions()
+{
+    $.getJSON( "https://automatic-disposer.000webhostapp.com/lss-bridge/available-missions.php", function( Response )
+    {
+        if( Response.status == "success" )
+        {
+            ADis_Available_Missions = Response.missions;
+        }
+    });
+}
+
+
+
+function ADis_FullAutomatic_CollectMissions()
+{
+    var Missions = JSON.parse( localStorage.getItem("AutomaticDispose-Missions") );
+    
+    if( Missions == null )
+        Missions = {};
+    
+    $("#mission_list").find(".missionSideBarEntry").each(function()
+    {
+        var MissionID = $(this).attr("mission_id");
+        var MissionType = $(this).attr("mission_type_id");
+        var MissionName = $(this).find(".map_position_mover").text();
+
+        if( typeof Missions[ MissionID ] == "undefined" && typeof ADis_Available_Missions[ MissionType ] != "undefined" )
+        {
+            AutomaticDispose_AddMission( MissionID, "full" );
+        }
+    });
+    
+    $("#mission_list_krankentransporte").find(".missionSideBarEntry").each(function()
+    {
+        var MissionID = $(this).attr("mission_id");
+        var MissionType = $(this).attr("mission_type_id");
+        var MissionName = $(this).find(".map_position_mover").text();
+
+        if( typeof Missions[ MissionID ] == "undefined" && typeof ADis_Available_Missions[ MissionType ] != "undefined" )
+        {
+            AutomaticDispose_AddMission( MissionID, "full" );
+        }
+    });
+}
