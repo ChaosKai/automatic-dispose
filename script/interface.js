a078c650088b79af28ae055e519866dfe2f9746c
ChaosKai <kai-waggeling@web.de>
2017-12-24 14:52:11 +0100

    Update interface.js

--- script/interface.js
+++ script/interface.js
@@ -19,31 +19,7 @@ $(document).ready(function()
 function ADis_CreateDashboard()
 {
     $('#news_li').before('<li id="adis-nav-item"></li>');
-    $('#adis-nav-item').load( AutomaticDispose_URL + AutomaticDispose_Branch + "/html/navbar.html" );
-    $("#adis-dashboard").css({ display: "none", opacity: "0" });
-
-    $("#adis-open-button").click(function()
-    {
-        $("#adis-dashboard").css("display", "flex");
-        $("#adis-dashboard").animate( { opacity: 1 }, 200 );
-    });
-
-    $("#adis-dashboard-close-button").click(function()
-    {
-        $("#adis-dashboard").animate( { opacity: 0 }, 200, function()
-        {
-            $(this).css("display", "none");
-        });
-    });
-
-    $("#adis-dashboard-switch-mode-button").click(function()
-    {
-        AutomaticDispose_SwitchMode();
-    });
-
-    $("#adis-dashboard-open-dispatcher-button").click(function()
-    {
-        $("#adis-dispatcher-overview").css("display", "flex");
-        $("#adis-dispatcher-overview").animate({ opacity: 1 }, 200);
-    });
+    $('#adis-nav-item').load( AutomaticDispose_URL + AutomaticDispose_Branch + "/html/navbar.html", function() {
+        $("#adis-dashboard").css({ display: "none", opacity: "0" });
+    } );
 }
