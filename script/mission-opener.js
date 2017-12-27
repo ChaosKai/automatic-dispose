933754e22a1a28b6bf8bfb3e13691a30e45c5c79
ChaosKai <kai-waggeling@web.de>
2017-12-27 18:55:54 +0100

    Update mission-opener.js

--- script/mission-opener.js
+++ script/mission-opener.js
@@ -11,17 +11,26 @@ function ADis_CheckMissionAttention()
     var Dispatchers = JSON.parse( localStorage.getItem("ADis-Dispatchers") );
     var Missions    = JSON.parse( localStorage.getItem("AutomaticDispose-Missions") );
     var CurrentTime = Math.floor( new Date().getTime() / 1000 );
+    
+    console.log("-------------------------------------------------------------------------------");
+    console.log("    ADis - Check Mission Attention");
+    console.log("");
        
     $.each(Missions, function(MissionID, Mission)
     {
-        if( $("#mission_" + MissionID).length == 0 )
+        console.log("      Mission: " + Mission.id + " " + Mission.name + ":");
+        
+        if( $("#mission_" + MissionID).length == 0 || Mission.mode != localStorage.getItem("AutomaticDispose-Mode") )
         {
             delete Missions[MissionID]
+            console.log("      - gelöscht");
         }
         else if( Mission.next_check < CurrentTime )
         {
+            console.log("      - braucht aufmerksamkeit");
             if( !Missions[MissionID].dispatcher && typeof ADis_Available_Missions[Missions[MissionID].type] !== "undefined" )
             {
+                console.log("      - wird vermittelt...");
                 $.each(Dispatchers, function(DispatcherID, Dispatcher)
                 {
                     // count missions with this dispatcher and validate
@@ -34,15 +43,21 @@ function ADis_CheckMissionAttention()
                             Num_Dispatcher_Missions++;
                         }
                     });
+                    
+                    console.log("      - Anzahl der Disponenten: " + Num_Dispatcher_Missions);
 
                     if( Dispatchers[DispatcherID].state && ADis_Available_Missions[Missions[MissionID].type].type == Dispatchers[DispatcherID].org )
                     {
                         Missions[ MissionID ].dispatcher = DispatcherID;
+                        console.log("      - an Dispontent " + DispatcherID + " übergeben");
                     }
                 });
             }
             else if( Missions[MissionID].dispatcher != false && $("#adis_dispatcher_workstation_" + Missions[MissionID].dispatcher).find("iframe").data("mission") == "empty" )
             {
+                console.log("      - bereits disponiert");
+                console.log("      - öffnen zum alarmieren");
+                
                 $("#adis_dispatcher_workstation_" + Missions[MissionID].dispatcher).find("iframe").attr("src", "https://www.leitstellenspiel.de/missions/" + Mission.id);
                 $("#adis_dispatcher_workstation_" + Missions[MissionID].dispatcher).find("iframe").data("mission", Mission.id);
                 
@@ -54,6 +69,7 @@ function ADis_CheckMissionAttention()
             else
             {
                 Missions[ MissionID ].dispatcher = false;
+                console.log("      - von Disponent entfernt");
             }
         }
     });
