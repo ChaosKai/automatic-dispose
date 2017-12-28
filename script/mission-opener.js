8f383dd6c8275f7284b0f5d04ba58a7a8e9c1c93
ChaosKai <kai-waggeling@web.de>
2017-12-28 17:41:49 +0100

    Update mission-opener.js

--- script/mission-opener.js
+++ script/mission-opener.js
@@ -18,24 +18,15 @@ var MissionFrameWatchDog = {};
         var Missions    = JSON.parse( localStorage.getItem("AutomaticDispose-Missions") );
         var Dispatchers = JSON.parse( localStorage.getItem("ADis-Dispatchers") );
 
-        console.log("-------------------------------------------------------------------------------");
-        console.log("    ADis - Asign Missions to Dispatchers");
-        console.log("");
-
         $.each(Missions, function(MissionID, Mission)
         {
             if( Missions[MissionID].dispatcher != false && !Dispatchers[ Missions[MissionID].dispatcher ].state  )
             {
-                console.log("      Mission: " + Mission.id + " " + Mission.name + ":");
-                console.log("      - wird freigestellt");
-                
                 Missions[ MissionID ].dispatcher = false;
             }
             
             if( !Missions[MissionID].dispatcher )
             {
-                console.log("      Mission: " + Mission.id + " " + Mission.name + ":");
-                console.log("      - wird vermittelt...");
                 $.each(Dispatchers, function(DispatcherID, Dispatcher)
                 {
                     // count missions with this dispatcher and validate
@@ -44,17 +35,12 @@ var MissionFrameWatchDog = {};
                     $.each(Missions, function(SubMissionID, SubMission)
                     {
                         if( Missions[SubMissionID].dispatcher == Dispatcher.id )
-                        {
                             Num_Dispatcher_Missions++;
-                        }
                     });
                     
-                    console.log("      - Anzahl der Disponenten: " + Num_Dispatcher_Missions);
-
                     if( Num_Dispatcher_Missions == 0 && Dispatchers[DispatcherID].state && ADis_Available_Missions[Missions[MissionID].type].type == Dispatchers[DispatcherID].org && !Missions[MissionID].dispatcher )
                     {
                         Missions[ MissionID ].dispatcher = DispatcherID;
-                        console.log("      - an Dispontent " + DispatcherID + " übergeben");
                     }
                 });
             }
@@ -72,30 +58,22 @@ function ADis_CheckMissionAttention()
     var Missions    = JSON.parse( localStorage.getItem("AutomaticDispose-Missions") );
     var CurrentTime = Math.floor( new Date().getTime() / 1000 );
     
-    console.log("-------------------------------------------------------------------------------");
-    console.log("    ADis - Check Mission Attention");
-    console.log("");
-       
     $.each(Missions, function(MissionID, Mission)
     {
         if( Mission.next_check > CurrentTime )
             return true;
         
-        console.log("      Mission: " + Mission.id + " " + Mission.name + ":");
+        if( !Missions[MissionID].dispatcher )
+            return true;
         
         if( $("#mission_" + MissionID).length == 0 || Mission.mode != localStorage.getItem("AutomaticDispose-Mode") || typeof ADis_Available_Missions[Missions[MissionID].type] === "undefined" )
         {
             delete Missions[MissionID]
-            console.log("      - gelöscht");
         }
         else if( Mission.next_check < CurrentTime )
         {
-            console.log("      - braucht aufmerksamkeit");
             if( Missions[MissionID].dispatcher != false && $("#adis_dispatcher_workstation_" + Missions[MissionID].dispatcher).find("iframe").data("mission") == "empty" )
             {
-                console.log("      - bereits disponiert");
-                console.log("      - öffnen zum alarmieren");
-                
                 $("#adis_dispatcher_workstation_" + Missions[MissionID].dispatcher).find("iframe").attr("src", "https://www.leitstellenspiel.de/missions/" + Mission.id);
                 $("#adis_dispatcher_workstation_" + Missions[MissionID].dispatcher).find("iframe").data("mission", Mission.id);
                 
