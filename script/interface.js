22e124608908d93cb9098172b92067fbc571a3da
ChaosKai <kai-waggeling@web.de>
2017-12-29 14:43:46 +0100

    Update interface.js

--- script/interface.js
+++ script/interface.js
@@ -1,6 +1,7 @@
 $(document).ready(function()
 {
     ADis_CreateDashboard();
+    ADis_CreateIframes();
     
     console.log("  Automatic Dispose: UI geladen");
 });
@@ -12,4 +13,12 @@ function ADis_CreateDashboard()
     $('#adis-nav-item').load( AutomaticDispose_URL + AutomaticDispose_Branch + "/html/navbar.html", function() {
         $("#adis-dashboard").css({ display: "none", opacity: "0" });
     } );
+}
+
+function ADis_CreateIframes()
+{
+    $("body").append('<div id="adis-iframe-container"></div>');
+    $("#adis-iframe-container").load( AutomaticDispose_URL + AutomaticDispose_Branch + "/html/iframes.html" );
+    
+    $("#adis-iframe-container").css({ display: "none", opacity: "0" });
 }
