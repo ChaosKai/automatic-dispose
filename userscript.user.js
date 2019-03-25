2efc9a6009ab0092e1f7287778ade11d065ddff0
ChaosKai <kai-waggeling@web.de>
2019-03-25 17:41:53 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -11,5 +11,5 @@
 
 var scriptElement = document.createElement("script");
 scriptElement.type = "text/javascript";
-scriptElement.src = "https://rawgit.com/ChaosKai/automatic-dispose/master/autoload.js";
+scriptElement.src = "https://chaoskai.github.io/automatic-dispose/autoload.js";
 document.body.appendChild(scriptElement);
