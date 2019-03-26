89124fb2ec7b8834501d91c5ff6d992ea3c5dc63
ChaosKai <kai-waggeling@web.de>
2019-03-27 00:11:04 +0100

    Update mission-windows.js

--- scripts/mission-windows.js
+++ scripts/mission-windows.js
@@ -1,5 +1,6 @@
 
-    var MissionWindows = [];
+    var Windows     = [];
+    var WindowQueue = [];
 
 //  - --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 //  -
@@ -9,19 +10,129 @@
     $(document).ready( function()
     {
         $("#col_navbar_holder").after(
-            $(`<div id="automatic-dispose-mission-windows"></div>`)
+            $(`<div id="automatic-dispose-windows"></div>`)
         );
         
-        for( var missionWindowIndex = 1; missionWindowIndex <= 5; missionWindowIndex++ )
+        for( var WindowIndex = 1; WindowIndex <= 5; WindowIndex++ )
         {
-            var MissionWindow = $(`<div class="mission-window"></div>`);
-            MissionWindow.attr("data-window", missionWindowIndex);
-            MissionWindow.attr("data-mission", "none");
-            MissionWindow.attr("data-vehicle", "none");
-            MissionWindow.attr("data-building", "none");
-            MissionWindow.append( $("<iframe></iframe>") );
+            var Window = $(`<div class="window"></div>`);
+            Window.attr("data-window", WindowIndex);
+            Window.attr("data-mission", "none");
+            Window.attr("data-vehicle", "none");
+            Window.attr("data-building", "none");
+            Window.append( $("<iframe></iframe>") );
             
-            $("#automatic-dispose-mission-windows").append(MissionWindow);
-            MissionWindows.push( MissionWindow );
+            $("#automatic-dispose-windows").append( Window );
+            Windows.push( Window );
         }
+        
+        setInterval( function()
+        {
+            executeNextQueue();
+        }, 1000);
     });
+
+
+
+//  - --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+//  -
+//  -           Mission Windows   |   Queue Functions
+//  - --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    function openVehicleWindow( VehicleId )
+    {
+        var AlreadyExisting = false;
+        
+        for( var QueueIndex = 0; QueueIndex < WindowQueue.length; QueueIndex++ )
+        {
+            if( WindowQueue[QueueIndex].type == "vehicle" && WindowQueue[QueueIndex].vehicle == VehicleId )
+            {
+                AlreadyExisting = true;
+            }
+        }
+        
+        if( !AlreadyExisting )
+        {
+            WindowQueue.push({
+                type    : "vehicle",
+                vehicle : VehicleId
+            })
+        }
+    }
+
+
+
+    function openMissionWindow( MissionId )
+    {
+        var AlreadyExisting = false;
+        
+        for( var QueueIndex = 0; QueueIndex < WindowQueue.length; QueueIndex++ )
+        {
+            if( WindowQueue[QueueIndex].type == "mission" && WindowQueue[QueueIndex].mission == MissionId )
+            {
+                AlreadyExisting = true;
+            }
+        }
+        
+        if( !AlreadyExisting )
+        {
+            WindowQueue.push({
+                type    : "mission",
+                vehicle : MissionId
+            })
+        }
+    }
+
+
+
+    function executeNextQueue()
+    {
+        if( WindowQueue.length == 0 )
+        {
+            return;
+        }
+        
+        var FreeWindow = -1;
+        
+        for( var WindowIndex = 0; WindowIndex < Windows.length; WindowIndex++ )
+        {
+            var Window = $("#automatic-dispose-windows").find(".window").eq(WindowIndex);
+            
+            if( Window.attr("data-mission") == "none" && 
+                Window.attr("data-vehicle") == "none" && 
+                Window.attr("data-building") == "none" && 
+                FreeWindow < 0 )
+            {
+                FreeWindow = WindowIndex;
+            }
+        }
+        
+        if( FreeWindow > -1 )
+        {
+            if( WindowQueue[0].type == "mission" )
+            {
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).find("iframe").attr("src", `https://www.leitstellenspiel.de/missions/${WindowQueue[0].mission}`);
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).attr("data-mission", `${WindowQueue[0].mission}`);
+            }
+            
+            if( WindowQueue[0].type == "vehicle" )
+            {
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).find("iframe").attr("src", `https://www.leitstellenspiel.de/vehicles/${WindowQueue[0].vehicle}`);
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).attr("data-vehicle", `${WindowQueue[0].mission}`);
+            }
+            
+            if( WindowQueue[0].type == "building" )
+            {
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).find("iframe").attr("src", `https://www.leitstellenspiel.de/buildings/${WindowQueue[0].vehicle}`);
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).attr("data-building", `${WindowQueue[0].mission}`);
+            }
+                
+            setTimeout( function()
+            {
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).find("iframe").attr("src", ``);
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).attr("data-mission", `none`);
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).attr("data-vehicle", `none`);
+                $("#automatic-dispose-windows").find(".window").eq(FreeWindow).attr("data-building", `none`);
+            }, 5000);
+        }
+    }
